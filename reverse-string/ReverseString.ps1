becfffd87a32c1871e530c47647ad3dc6431690f
Abhishek Sharma <abhishdevops@gmail.com>
2021-04-04 04:23:40 +0530

    hello-world,reverse string

---
+++ reverse-string/ReverseString.ps1
@@ -0,0 +1,40 @@
+Function Get-ReverseString {
+    <#
+    .SYNOPSIS
+    Reverse a string
+
+    .DESCRIPTION
+    Reverses the string in its entirety. That is it does not reverse each word in a string individually.
+
+    .PARAMETER Forward
+    The string to be reversed
+
+    .EXAMPLE
+    Get-ReverseString "PowerShell"
+    
+    This will return llehSrewoP
+
+    .EXAMPLE
+    Get-ReverseString "racecar"
+
+    This will return racecar as it is a palindrome
+    #>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Position=1, ValueFromPipeline=$true)]
+        [string]$Forward
+
+	)
+
+    
+    
+	
+    $reverse = ""
+    for ($i = $Forward.Length; $i -ge 0; $i--) {
+        $reverse += $Forward[$i]
+    }
+    return $reverse
+	#Throw "Function not implemented"
+}
+
+Get-ReverseString -Forward "Ramen"
